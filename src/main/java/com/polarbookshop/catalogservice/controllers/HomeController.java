338bda107d7a5f65fc6be108ac7798b97dc16ccd
dphresh77 <dphresh657@gmail.com>
2023-06-22 12:16:30 -0500

    Catalog-service repo

--- src/main/java/com/polarbookshop/catalogservice/controllers/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
+    //
     @GetMapping("/")
     public String getGreeting() {
         return "Welcome to the book catalog!";
