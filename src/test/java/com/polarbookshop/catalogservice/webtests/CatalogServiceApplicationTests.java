c46e7b8c6611c92ee25cb14d73ccaf2f50e5e05e
dphresh77 <dphresh657@gmail.com>
2023-07-16 17:31:10 -0500

    Fix tests, Add Book Repo Jdbc test

--- src/test/java/com/polarbookshop/catalogservice/webtests/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/webtests/CatalogServiceApplicationTests.java
@@ -5,11 +5,13 @@ import com.polarbookshop.catalogservice.domain.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 
 import java.time.Instant;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -18,17 +20,15 @@ import static org.mockito.BDDMockito.willReturn;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //Servlet Container listening on random port
+@ActiveProfiles("integration")
 public class CatalogServiceApplicationTests {
 
     @Autowired
     private WebTestClient webTestClient;
 
-    @Autowired
-    BookService bookService;
-
     @Test
     void whenPostRequestThenBookCreated() {
-        var expectedBook = new Book(1L,"1231231231", "Title", "Author", 9.90, Instant.now(), Instant.now(), 1);
+        var expectedBook = Book.of("1231231231", "Title", "Author", 9.90);
 
         webTestClient
                 .post() //HTTP Request to send
@@ -43,20 +43,31 @@ public class CatalogServiceApplicationTests {
                 });
     }
 
-/*    @Test
-    void whenPutRequestThenIsOk(){
-        var bookToBeUpdated = new Book("1234567890", "Title","Author", 10.00);
-        var updatedBook = new Book("1234567890", "newTitle", "newAuthor", 1.00);
+   @Test
+    void whenPutRequestThenBookUpdated(){
+        var bookIsbn = "1231231232";
+        var bookToCreate = Book.of(bookIsbn, "Title", "Author", 9.90);
+        Book createdBook = webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat( book).isNotNull())
+                .returnResult().getResponseBody();
+
+        var bookToUpdate = new Book(createdBook.id(), createdBook.isbn(), createdBook.title(), createdBook.author(),
+                7.95, createdBook.createdDate(), createdBook.lastModifiedDate(), createdBook.version());
 
         webTestClient
                 .put()
-                .uri("/books/1234567890")
-                .bodyValue(updatedBook)
+                .uri("/books/" + bookIsbn)
+                .bodyValue(bookToUpdate)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Book.class).value(actualBook -> {
                     assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.price()).isEqualTo(bookToUpdate.price());
                 });
-
-    }*/
+    }
 }
